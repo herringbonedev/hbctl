16dea8e2bedd8f0405900edc09e1c6db468954c0
unixv <andrewcampagnaservices@gmail.com>
2026-02-25 21:09:57 -0600

    fix version

--- cmd/version.go
+++ cmd/version.go
@@ -2,11 +2,7 @@ package cmd
 
 import "fmt"
 
-<<<<<<< HEAD
-var Version = "alpha-0.1.0"
-=======
 var Version = "alpha-0.2.0"
->>>>>>> 758a020 (include herringbone-proxy)
 
 func init() {
 	Register("version", versionCmd)
